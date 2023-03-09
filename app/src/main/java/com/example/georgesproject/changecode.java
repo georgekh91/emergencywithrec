c5ce615c92519e439eaf25224c28b02af767eb94
unknown <george@DESKTOP-24716.24716>
2023-03-09 23:21:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/georgesproject/changecode.java
@@ -0,0 +1,14 @@
+package com.example.georgesproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class changecode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changecode);
+    }
+}
